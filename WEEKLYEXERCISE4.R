6c90ad4ac7ba1d155fd7c411d5715f4a67b54c6d
Austin <austinzepeda1@gmail.com>
2025-09-24 14:01:39 -0500

    answered 6a.

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -1,3 +1,20 @@
 library(usethis)
 git_default_branch()
 # [1] "main"  - This is what we're looking for!
+
+# Step 6 - using Olympics.csv
+
+library(readr)
+library(tidyverse)
+library(ggplot2)
+
+olympics = read_csv("Olympics.csv")
+
+class(olympics$gold)
+class(olympics$silver)
+class(olympics$bronze) # all numeric classes. I should be able to just sum them.
+
+olympics2 = olympics %>%
+  mutate(total.medals = gold + silver + bronze)
+
+view(olympics2) # the column is present and accurate.
