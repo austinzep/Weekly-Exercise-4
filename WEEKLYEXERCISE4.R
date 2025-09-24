e84c6049b5de13d5d6f9be840f0c5461223ba5d8
Austin <austinzepeda1@gmail.com>
2025-09-24 14:29:56 -0500

    answered 7a.

--- WEEKLYEXERCISE4.R
+++ WEEKLYEXERCISE4.R
@@ -1,3 +1,15 @@
 library(usethis)
 git_default_branch()
 # [1] "main"  - This is what we're looking for!
+
+library(tidyverse)
+
+olympics = read_csv("Olympics.csv")
+
+largest_delegations = olympics %>%
+  filter(year == 1992) %>%
+  select(country, athletes) %>%
+  arrange(desc(athletes))
+
+str(largest_delegations) #it's a tibble
+view(largest_delegations)
